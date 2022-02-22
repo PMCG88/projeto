6e343ac25b45458cb352b2ea548a1d7924c44226
pedromcgandra <pedromcgandra@gmail.com>
2022-02-22 23:00:45 +0000

    add options accordion logic

--- js/options.js
+++ js/options.js
@@ -1,13 +1,18 @@
 const options = document.querySelector(".options");
+const contents = document.querySelectorAll(".options__content");
 
 options.addEventListener("click", (event) => {
   const header = event.target.closest(".options__header");
-  header.parentElement.classList.toggle(".options__option--open");
+  header.parentElement.classList.toggle("options__option--open");
 });
 
-const contents = document.querySelectorAll(".options__content");
+window.addEventListener("resize", setHeight);
 
-contents.forEach((content) => {
-  const height = content.firstElementChild.offsetHeight;
-  content.style.setProperty("--option-height", height + 0.1 + "px");
-});
+setHeight();
+
+function setHeight() {
+  contents.forEach((content) => {
+    const height = content.firstElementChild.offsetHeight;
+    content.style.setProperty("--option-height", height + 0.01 + "px");
+  });
+}
